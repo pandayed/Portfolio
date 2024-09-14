3793160923c58ed319771bdc100aad0ab1b11994
pandayed <lalbiharipandeyg@gmail.com>
2024-09-14 16:20:56 +0530

    home page done.

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -2,13 +2,58 @@
 import React from "react";
 import './Footer.css';  // Create a CSS file for styling
 import '../CommonClasses/CommonClasses.css';
+import UprightArrow from '../assets/icons/upright_arrow.svg';
+
+
+const SocialLink = (props) => {
+    return (
+        <li className="SocialLinkItem">
+            <a className="SiteLink" href={props.link} >
+                {/* <img src={UprightArrow} className="SocialIcon" alt="icon" /> */}
+                <div className="SocialText">{props.title}</div>
+            </a>
+        </li>
+    );
+}
+
 
 const Footer = () => {
     return (
         <footer className="Footer">
-            <div className="logo">
-                <a href="/">pandayed.com</a>
+
+            <div className="FooterBody">
+
+                <div className="Socials">
+                    <div className="SocialsTitle">
+                        Socials
+                    </div>
+
+                    <ul className="SocialLinks">
+                        <SocialLink title="Instagram" link="" />
+                        <SocialLink title="Twitter/X" link="" />
+                    </ul>
+                    <div>
+
+                    </div>
+
+                    <ul className="SocialLinks">
+                        <SocialLink title="G-Mail" link="" />
+                        <SocialLink title="LinkedIn" link="" />
+                        <SocialLink title="GitHub" link="" />
+
+                    </ul>
+
+                </div>
+
             </div>
+
+            <div className="FooterBase">
+                <div className="logo">
+                    <a href="/">pandayed.com</a>
+                </div>
+            </div>
+
+
         </footer>
     );
 };
