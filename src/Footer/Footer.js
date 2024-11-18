f0e068069219ad8c59d6e24d1a634af15292f96e
Lal Bihari Pandey <lalpande@cisco.com>
2024-11-18 21:17:47 +0530

    UI updated

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -12,12 +12,10 @@ import GitHub from '../assets/social_icons/icons8-github.svg';
 
 const SocialLink = (props) => {
     return (
-        <li className="SocialLinkItem">
             <a className="SiteLink" href={props.link} >
                 {/* <img src={props.icon} className="SocialIcon" alt="icon" /> */}
                 <div className="SocialTitle">{props.title}</div>
             </a>
-        </li>
     );
 }
 
@@ -29,24 +27,20 @@ const Footer = () => {
             <div className="FooterBody">
 
                 <div className="Socials">
-                    {/* <div className="SocialsTitle">
-                        Socials
-                    </div> */}
 
-                    <ul className="SocialLinks">
-                        <SocialLink title="Instagram" link="" icon={InstagramIcon} />
-                        <SocialLink title="Twitter/X" link="" icon={TwitterX} />
-                    </ul>
-                    <div>
-
-                    </div>
-
-                    <ul className="SocialLinks">
+                    <div className="SocialLinks">
                         <SocialLink title="G-Mail" link="" icon={Gmail} />
                         <SocialLink title="LinkedIn" link="" icon={LinkedIn} />
                         <SocialLink title="GitHub" link="" icon={GitHub} />
 
-                    </ul>
+                    </div>
+
+                    <div className="SocialLinks">
+                        <SocialLink title="Instagram" link="" icon={InstagramIcon} />
+                        <SocialLink title="Twitter/X" link="" icon={TwitterX} />
+                    </div>
+
+                    
 
                 </div>
 
