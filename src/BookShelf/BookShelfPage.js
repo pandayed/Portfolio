eb864b79fc8980571244dd06bc88d7142bb86ec1
Lal Bihari Pandey <lalpande@cisco.com>
2024-11-29 22:50:05 +0530

    UI changes, but not working.

--- src/BookShelf/BookShelfPage.js
+++ src/BookShelf/BookShelfPage.js
@@ -2,59 +2,78 @@ import React, { useState } from "react";
 import { getBookCoverUrl } from "../api-calls";
 import './BookShelf.css';
 
-const books = [
+const booksByCategory = [
     {
-        name: "Rich Dad Poor Dad",
-        author: "Robert Kiyosaki",
-        isbn: "9781612681139"
+        category: "Self Help",
+        booksList: [
+            {
+                "name": "Rich Dad Poor Dad",
+                "author": "Robert Kiyosaki",
+                "isbn": "9781612681139"
+            },
+            {
+                "name": "Subtle Art of Not Giving a F*ck",
+                "author": "Mark Manson",
+                "isbn": "9780062457714"
+            },
+            {
+                "name": "Atomic Habits",
+                "author": "James Clear",
+                "isbn": "9780735211292"
+            },
+            {
+                "name": "Ikigai",
+                "author": "Héctor García, Francesc Miralles",
+                "isbn": "9780143130727"
+            }
+        ]
     },
     {
-        name: "The Almanac Of Naval Ravikant",
-        author: "Naval Ravikant",
-        isbn: "9789698729707"
+        category: "Biography",
+        booksList: [
+            {
+                "name": "Autobiography of a Yogi",
+                "author": "Paramahansa Yogananda",
+                "isbn": "9780876120798"
+            }
+        ]
     },
     {
-        name: "Subtle Art of Not Giving a F*ck",
-        author: "Mark Manson",
-        isbn: "9780062457714"
+        category: "Finance/Investment",
+        booksList: [
+            {
+                "name": "The Intelligent Investor",
+                "author": "Benjamin Graham",
+                "isbn": "9780060555665"
+            },
+            {
+                "name": "Coffee Can Investing",
+                "author": "Saurabh Mukherjea",
+                "isbn": "9780670090457"
+            },
+            {
+                "name": "The Psychology of Money",
+                "author": "Morgan Housel",
+                "isbn": "9780857197689"
+            }
+        ]
     },
     {
-        name: "The Intelligent Investor",
-        author: "Benjamin Graham",
-        isbn: "9780060555665"
-    },
-    {
-        name: "The Alchemist",
-        author: "Paulo Coelho",
-        isbn: "9780061122415"
-    },
-    {
-        name: "Atomic Habits",
-        author: "James Clear",
-        isbn: "9780735211292"
-    },
-    {
-        name: "Autobiography of a Yogi",
-        author: "Paramahansa Yogananda",
-        isbn: "9780876120798"
-    },
-    {
-        name: "Coffee Can Investing",
-        author: "Saurabh Mukherjea",
-        isbn: "9780670090457"
-    },
-    {
-        name: "The Psychology of Money",
-        author: "Morgan Housel",
-        isbn: "9780857197689"
-    },
-    {
-        name: "Ikigai",
-        author: "Héctor García, Francesc Miralles",
-        isbn: "9780143130727"
+        category: "Philosophy/Personal Growth",
+        booksList: [
+            {
+                "name": "The Almanac Of Naval Ravikant",
+                "author": "Naval Ravikant",
+                "isbn": "9789698729707"
+            },
+            {
+                "name": "The Alchemist",
+                "author": "Paulo Coelho",
+                "isbn": "9780061122415"
+            }
+        ]
     }
 ];
-
 
 
 
@@ -81,7 +100,7 @@ const ImageDisplay = ({ isbn }) => {
 
     return (
         <div>
-            {image && <img src={image} alt="Book Cover" className="BookImage"/>}
+            {image && <img src={image} alt="Book Cover" className="BookImage" />}
         </div>
     );
 };
@@ -90,16 +109,31 @@ export const BookShelfPage = () => {
     return (
         <div className="BookShelf">
             <div className="SectionTitle">Bookshelf</div>
-            <div className="BooksList">
-                {books.map((book, index) => (
-                    <div key={index} className="Book">
-                        {/* <p className="BookName">{book.name}</p> */}
-                        <ImageDisplay isbn={book.isbn} />
-                        {/* <p>Author: {book.author}</p> */}
+
+            {
+                booksByCategory.map((category, index) => {
+                    <div className="BooksList">
+
+                        <p className="CategoryTitle">
+                            {"hi"}
+                        </p>
+
+                        <div >
+                            {category.booksList.map((book, _) => (
+                                <div key={index} className="Book">
+                                    <p className="BookName">{book.name}</p>
+                                    <ImageDisplay isbn={book.isbn} />
+                                    <p>Author: {book.author}</p>
+                                </div>
+                            ))}
+                        </div>
+
                     </div>
-                ))}
-            </div>
-            
+                })
+            }
+
+
+
         </div>
     );
 };
