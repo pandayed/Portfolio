a5051dc1f4cccb7e3b8afcb1415dfe609b02ff01
Lal Bihari Pandey <lalbiharipandeyg@gmail.com>
2024-11-29 23:20:19 +0530

    UI Changes.

--- src/Projects/ProjectsPage.js
+++ src/Projects/ProjectsPage.js
@@ -1,4 +1,4 @@
-import React from "react"; 
+import React from "react";
 import './ProjectsPage.css';
 import { Technologies } from "../common";
 import PortfolioImage from '../assets/project-images/portfolio.png';
@@ -7,7 +7,7 @@ import GitHub from '../assets/common-icons/ic_github.svg';
 const RepoVisibility = (props) => {
     return (
         <div className="RepoVisibility">
-            <img src={GitHub} className="VisibilityIcon"/>
+            <img src={GitHub} className="VisibilityIcon" />
             <p className="VisibilityText">{props.visibility}</p>
         </div>
     );
@@ -16,7 +16,8 @@ const RepoVisibility = (props) => {
 const Projects = [
     {
         title: "Portfolio",
-        url: "https://pandayed.github.io/Portfolio/",
+        deployment: "https://pandayed.github.io/Portfolio/",
+        github: "https://github.com/pandayed/Portfolio",
         technologies: [
             "React",
             "JavaScript"
@@ -25,27 +26,35 @@ const Projects = [
         projectImage: PortfolioImage,
         visibility: "Public"
     },
-    {
-        title: "EdPub",
-        url: "https://www.linkedin.com/in/pandayed/",
-        technologies: [
-            "Kotlin",
-            "Android",
-            "Firebase"
-        ],
-        visibility: "Private"
-    },
-    {
-        title: "Epione",
-        url: "",
-        technologies: [
-            "Flutter",
-            "Android",
-            "Firebase"
-        ],
-        visibility: "Private"
-    }
+    // {
+    //     title: "EdPub",
+    //     url: "https://www.linkedin.com/in/pandayed/",
+    //     technologies: [
+    //         "Kotlin",
+    //         "Android",
+    //         "Firebase"
+    //     ],
+    //     visibility: "Private"
+    // },
+    // {
+    //     title: "Epione",
+    //     url: "",
+    //     technologies: [
+    //         "Flutter",
+    //         "Android",
+    //         "Firebase"
+    //     ],
+    //     visibility: "Private"
+    // }
 ]
+
+const GitHubLink = (props) => {
+    return (
+        <a href={props.githubLink} target="_blank" className="GitHubLink">
+            {props.text}
+        </a>
+    )
+}
 
 const Project = (props) => {
     const { project } = props;
@@ -59,11 +68,15 @@ const Project = (props) => {
         >
             <div className="ProjectTextDetails">
                 <p className="ProjectTitle">{project.title} </p>
-                <RepoVisibility visibility={project.visibility} />
+                <div className="VisibilityAndLinks">
+                    <RepoVisibility visibility={project.visibility} />
+                    {project.github && <GitHubLink githubLink={project.github} text="View Repo"/>}
+                    {project.deployment && <GitHubLink githubLink={project.deployment} text="View Deoployment"/>}
+                </div>
                 <p className="ProjectDescription">{project.description}</p>
                 <Technologies technologies={project.technologies} />
             </div>
-            {project.projectImage && <img src={project.projectImage} className="ProjectImage"/>}
+            {project.projectImage && <img src={project.projectImage} className="ProjectImage" />}
         </a>
     );
 };
@@ -74,8 +87,8 @@ export const ProjectsPage = () => {
             <div className="SectionTitle">Featured Projects</div>
             <div className="ProjectsList">
                 {
-                    Projects.map((project, index)=>(
-                        <Project project={project}/>
+                    Projects.map((project, index) => (
+                        <Project project={project} />
                     ))
                 }
             </div>
